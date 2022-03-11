58ed24ce733faa314f9b393c5ec67caedf086924
ramon <ramona.schultze@web.de>
2022-03-11 10:34:57 +0100

    commit

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ name_age("Ramona", 21)
 def swap_integers(num1, num2):
     a = num1
     b = num2
+    print(a)
+    print(b)
     temp = a  # num1
     a = b  # num2
     b = temp  # num1
@@ -32,10 +34,11 @@ def check_numbers(num1, num2):
 print(check_numbers(6, 10))  # vorher hatte ich hier kein print, aber hab es nicht geschafft
                             # dass der return value angezeigt wird
 
+
 def sum_up(num1, num2):
-    num_list = range(num1, num2 + 1)
+    numbers = range(num1, num2 + 1)
     if num2 > num1 > 0:
-        result = sum(num_list)
+        result = sum(numbers)
         return result
     else:
         return 0
@@ -52,7 +55,7 @@ def circle_area(r1, r2, r3):
     print("a1:", a1)
     print("a2:", a2)
     print("a3:", a3)
-    return r1 + r2 + r3
+    return r1 + r2 + r3  # die Summe aller Radii ist nur ein Float, wenn die einzelenen Radii Floats sind
 
 
 print(circle_area(1, 2, 3))
